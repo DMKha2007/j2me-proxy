a2d526a69c9e0e75805ccf69d3e96bd9f3acdebd
DMKha <97138901+DMKha2007@users.noreply.github.com>
2025-04-11 20:58:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,12 +14,19 @@ app.get('/', (req, res) => {
 app.get('/proxy', async (req, res) => {
   const targetUrl = req.query.url;
   if (!targetUrl) return res.status(400).send('Thiếu URL!');
+  if (!targetUrl || !/^https?:\/\//.test(targetUrl)) {
+  return res.status(400).send('URL không hợp lệ!');
+  }
 
   try {
     const response = await axios.get(targetUrl, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-      }
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+      },
+      maxRedirects: 5
     });
     const $ = cheerio.load(response.data);
 
